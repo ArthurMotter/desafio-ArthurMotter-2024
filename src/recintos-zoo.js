ff10c50b2a23226a96bad59142d84bd3a40f1c2b
arthur motter <arthur.motter.fatec@gmail.com>
2024-09-07 13:42:47 -0300

    funcoes de validacao

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,26 +1,45 @@
 class RecintosZoo {
     constructor() {
-      this.recintos = [
-        { numero: 1, bioma: 'savana', tamanhoTotal: 10, animais: ['MACACO', 'MACACO', 'MACACO'] },
-        { numero: 2, bioma: 'floresta', tamanhoTotal: 5, animais: [] },
-        { numero: 3, bioma: 'savana e rio', tamanhoTotal: 7, animais: ['GAZELA'] },
-        { numero: 4, bioma: 'rio', tamanhoTotal: 8, animais: [] },
-        { numero: 5, bioma: 'savana', tamanhoTotal: 9, animais: ['LEAO'] },
-      ];
-      this.animaisConfig = {
-        LEAO: { tamanho: 3, biomas: ['savana'], tipo: 'carnivoro' },
-        LEOPARDO: { tamanho: 2, biomas: ['savana'], tipo: 'carnivoro' },
-        CROCODILO: { tamanho: 3, biomas: ['rio'], tipo: 'carnivoro' },
-        MACACO: { tamanho: 1, biomas: ['savana', 'floresta'], tipo: 'herbivoro' },
-        GAZELA: { tamanho: 2, biomas: ['savana'], tipo: 'herbivoro' },
-        HIPOPOTAMO: {
-          tamanho: 4,
-          biomas: ['savana', 'rio'],
-          tipo: 'herbivoro',
-          restricao: 'savanaerio',
-        },
-      };
+        this.recintos = [
+            { numero: 1, bioma: 'savana', tamanhoTotal: 10, animais: ['MACACO', 'MACACO', 'MACACO'] },
+            { numero: 2, bioma: 'floresta', tamanhoTotal: 5, animais: [] },
+            { numero: 3, bioma: 'savana e rio', tamanhoTotal: 7, animais: ['GAZELA'] },
+            { numero: 4, bioma: 'rio', tamanhoTotal: 8, animais: [] },
+            { numero: 5, bioma: 'savana', tamanhoTotal: 9, animais: ['LEAO'] },
+        ];
+        this.animaisConfig = {
+            LEAO: { tamanho: 3, biomas: ['savana'], tipo: 'carnivoro' },
+            LEOPARDO: { tamanho: 2, biomas: ['savana'], tipo: 'carnivoro' },
+            CROCODILO: { tamanho: 3, biomas: ['rio'], tipo: 'carnivoro' },
+            MACACO: { tamanho: 1, biomas: ['savana', 'floresta'], tipo: 'herbivoro' },
+            GAZELA: { tamanho: 2, biomas: ['savana'], tipo: 'herbivoro' },
+            HIPOPOTAMO: {
+                tamanho: 4,
+                biomas: ['savana', 'rio'],
+                tipo: 'herbivoro',
+                restricao: 'savanaerio',
+            },
+        };
     }
-  }
-  
-  export { RecintosZoo as RecintosZoo };
+    
+    animalValido(animal) {
+        return Object.keys(this.animaisConfig).includes(animal);
+    }
+
+    quantidadeValida(quantidade) {
+        return quantidade > 0;
+    }
+
+    biomaAdequado(animal, recinto) {
+        return this.animaisConfig[animal].biomas.includes(recinto.bioma);
+    }
+
+    espacoSuficiente(animal, quantidade, recinto) {
+        const espacoNecessario = this.animaisConfig[animal].tamanho * quantidade;
+        const espacoOcupado = recinto.animais.length;
+        return espacoNecessario + espacoOcupado <= recinto.tamanhoTotal;
+    }
+
+}
+
+export { RecintosZoo as RecintosZoo };
